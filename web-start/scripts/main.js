85748e2809ab6c760395abd92fe04b26ea422792
nellykelly <niceperson787@gmail.com>
2018-10-20 14:53:37 -0700

    Testing firbase integration

--- web-start/scripts/main.js
+++ web-start/scripts/main.js
@@ -13,6 +13,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+  var config = {
+  apiKey: "apiKey",
+  authDomain: "projectId.firebaseapp.com",
+  databaseURL: "https://databaseName.firebaseio.com",
+  storageBucket: "bucket.appspot.com"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
 'use strict';
 
 // Signs-in Friendly Chat.
