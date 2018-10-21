659094331016d7e3d7173907d50ff70bcd1f512a
javaughn-sanders <javaughnd.sanders@gmail.com>
2018-10-21 02:05:33 -0400

    Revert "fix errr0r"
    This reverts commit 862162dc13941a197884d26483dabe2f8a0b52b9.

--- web-start/public/scripts/main.js
+++ web-start/public/scripts/main.js
@@ -58,6 +58,7 @@ function signOut() {
   // TODO 2: Sign out of Firebase.
 }
 
+<<<<<<< HEAD
 function submit() {
 /*  alert(""+selectedType + selectedType1)
     return firebase.database().ref('/assignment/').push({
@@ -72,7 +73,8 @@ function submit() {
   alert("ok" + elementVal);*/
 }
 
-
+=======
+>>>>>>> b0586329c2099bba334d16fd1115f988f03f9407
 // Initiate firebase auth.
 function initFirebaseAuth() {
   firebase.auth().onAuthStateChanged(authStateObserver);
